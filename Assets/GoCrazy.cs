966bd3500c7992c4a02e51471fab416e99e931f6
BrianLam4101 <BrianLam4101@gmail.com>
2016-05-20 01:29:48 -0700

    Added Basic UI, Fixed Movement, and Moved Resources
    - Added in basic UI setup, this is just to have something there for now
    - Made all the sliders go crazy to show that they work
    - Fixed player movement, and by that I mean I replaced all my scripts
    with stuff unity already had in the default assets
    - Moved all resources into the resource folder, this allows everything
    to be accessed within scripts

---
+++ Assets/GoCrazy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoCrazy : MonoBehaviour {
+
+    Slider slider;
+    Text text;
+    float velocity;
+
+    // Use this for initialization
+    void Start () {
+        slider = GetComponent<Slider>();
+        text = GetComponentInChildren<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        velocity += Random.Range(-0.01f, 0.01f);
+        float value = slider.value + velocity;
+        if (value > 1) {
+            value = 1;
+            velocity = 0;
+        } else if (value < 0) {
+            value = 0;
+            velocity = 0;
+        }
+        slider.value = value;
+        text.text = ((int)(value * 100)).ToString();
+    }
+}
