213386fd2ac0f002c724a4242ab2c3307b012644
BrianLam4101 <BrianLam4101@gmail.com>
2016-05-25 03:38:36 -0700

    Added day night cycle and water gun
    - Sun will go down
    - right click to shoot water

---
+++ Assets/Resources/Scripts/sasdasdf.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sasdasdf : MonoBehaviour {
+
+    GameObject water;
+    StatHolder waterHolder;
+
+    // Use this for initialization
+    void Start () {
+        water = Resources.Load<GameObject>("water_temp");
+        waterHolder = GameObject.Find("WaterLevel").GetComponent<StatHolder>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Mouse1)) {
+            waterHolder.waterLevel -= 0.1f;
+            GameObject drop = Instantiate<GameObject>(water);
+            drop.transform.position = gameObject.transform.position;
+            drop.transform.rotation = gameObject.transform.rotation;
+            drop.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * 20, ForceMode.VelocityChange);
+        }
+	}
+}
