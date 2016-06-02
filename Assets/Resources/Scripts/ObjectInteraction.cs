2f930937c96cef50fe7405cae4ecdbfa1f05a9a2
BrianLam4101 <BrianLam4101@gmail.com>
2016-06-02 10:19:46 -0700

    Added interact icon when hover over Appliance

--- Assets/Resources/Scripts/ObjectInteraction.cs
+++ Assets/Resources/Scripts/ObjectInteraction.cs
@@ -4,17 +4,26 @@ using System.Collections;
 public class ObjectInteraction : MonoBehaviour {
     public float distance;
     private RaycastHit fphit;
+
+    public GameObject crossHair;
+    public GameObject hand;
+
 	// Update is called once per frame
 	void Update ()
     {
         var layerMask = (1 << 4) + (5 << 8);
-        if ((Physics.Raycast(this.transform.position, this.transform.forward, out fphit, distance, ~layerMask)) && (fphit.collider.gameObject.CompareTag("Appliance")) && Input.GetMouseButtonDown(0))
-        {
-            //Debug.Log("got an appliance and left-click" + " | " + fphit.collider.gameObject);
-            foreach (Appliance x in fphit.collider.GetComponents<Appliance>())
-            {
-                x.appUse();
+        if ((Physics.Raycast(this.transform.position, this.transform.forward, out fphit, distance, ~layerMask)) && (fphit.collider.gameObject.CompareTag("Appliance"))) {
+            crossHair.SetActive(false);
+            hand.SetActive(true);
+            if (Input.GetMouseButtonDown(0)) {
+                //Debug.Log("got an appliance and left-click" + " | " + fphit.collider.gameObject);
+                foreach (Appliance x in fphit.collider.GetComponents<Appliance>()) {
+                    x.appUse();
+                }
             }
+        } else {
+            crossHair.SetActive(true);
+            hand.SetActive(false);
         }
 	}
 }
---
+++ Assets/playAnimation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playAnimation : MonoBehaviour {
+
+    Sprite[] images;
+    Image image;
+
+    int frame;
+    float delay;
+
+	// Use this for initialization
+	void Start () {
+        images = Resources.LoadAll<Sprite>("interact");
+        image = GetComponent<Image>();
+        frame = 0;
+        delay = 0;
+
+    }
+
+    void Update () {
+        if (delay < Time.fixedTime) {
+            image.sprite = images[frame];
+            frame++;
+            if (frame >= images.Length)
+                frame = 0;
+            delay += 0.2f;
+        }
+    }
+}
