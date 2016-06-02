38373abfe4e2f54e11cdad9a619e3d6b62ce09c7
BrianLam4101 <BrianLam4101@gmail.com>
2016-06-02 12:45:06 -0700

    added credits screen

---
+++ Assets/ReturnMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenu : MonoBehaviour {
+
+
+    public void mainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
